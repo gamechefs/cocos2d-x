dd5f383399be00e833d2391dedb91c3e5ed701db
Omer Saeed <omer.saeed@gmail.com>
2013-12-18 13:43:51 +0500

    Used the Json Parser from cocos-ext rather than custom

--- extensions/cocos-ext.h
+++ extensions/cocos-ext.h
@@ -62,9 +62,9 @@
 //
 //#include "CocoStudio/GUI/System/CocosGUI.h"
 //
-//#include "CocoStudio/Json/CSContentJsonDictionary.h"
-//#include "CocoStudio/Json/DictionaryHelper.h"
-//
+#include "CocoStudio/Json/CSContentJsonDictionary.h"
+#include "CocoStudio/Json/DictionaryHelper.h"
+
 //#include "CocoStudio/Reader/SceneReader.h"
 //
 //#include "CocoStudio/Action/CCActionManager.h"
